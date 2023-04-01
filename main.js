c85ee7af232172ec3694e0c06c70446d61b2b287
NihithaAvula <86791582+NihithaAvula@users.noreply.github.com>
2023-04-01 11:22:58 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -4,9 +4,29 @@ function preload(){
 
 function setup(){
  canvas = createCanvas(500,500);
- canvas.center();
+ canvas.position(700,110);
+ video = createCapture(VIDEO);
+ video.size(500,500);
+ poseNet = ml5.poseNet(video,modelLoaded);
+ poseNet.on('pose',gotPoses);
+}
+
+function modelLoaded(){
+    console.log("poseNet is initialized");
 }
 
 function draw(){
+background("#FFFC2A");
+}
 
+
+function gotPoses(results){
+if(results.length> 0){
+    console.log(results);
+    console.log("noseX = " + results[0].pose.nose.x);
+    console.log("noseY = " + results[0].pose.nose.y);
+    console.log("rightWristX = " + results[0].pose.rightWrist.x);
+    console.log("leftWristX = " + results[0].pose.lefttWrist.x);
+
+}
 }
